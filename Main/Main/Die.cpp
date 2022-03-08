9596d9de91ecdeae155f40f6186a51033dadc5b4
Aaron Shek <78095030+ItsAAR0N@users.noreply.github.com>
2022-03-08 19:05:15 +0000

    Delete Main directory

--- Main/Main/Die.cpp
+++
@@ -1,9 +0,0 @@
-#include "Die.h"
-
-void Die::setSimulateDie_no() {
-	currentNo = randomNo[rand() % 3]; // Between 3,5,7
-}
-
-void Die::setSimulateDie_col() {
-	currentCol = randomCol[rand() % 2]; // Between White, black
-}

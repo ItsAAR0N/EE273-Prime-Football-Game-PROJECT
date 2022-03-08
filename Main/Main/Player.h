6225688977a998398d75e098fa2b0dfc3d183edb
Aaron Shek <78095030+ItsAAR0N@users.noreply.github.com>
2022-03-08 19:06:02 +0000

    Add files via upload

---
+++ Main/Main/Player.h
@@ -0,0 +1,17 @@
+#ifndef Player_H
+#define Player_H
+
+
+class Player {
+private:
+	int primeNumber;
+public:
+	// Getters
+	int getprime() { return this->primeNumber; }
+	// Setters
+	void setPrime(int prime);
+
+};
+
+
+#endif
